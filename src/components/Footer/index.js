95c25d7ef3c225429440c33b007d5982c5a5b01b
Juan Carlos Quiros <juan.kqm96@gmail.com>
2021-01-12 20:02:38 -0600

    Footer added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 const StyledFooter = styled.footer`
+	display:flex;
+	align-items:flex-end;
+	justify-content:center;
 	background: #2c2c44;
 	width: 100%;
 	height: 6vh;
 	color: white;
 	text-align: center;
-	font-weight: 300;	
+	font-weight: 300;
+		strong{
+			color:#11bcca;
+			font-weight: bold;
+		}	
 	}
 `;
 export default function Footer() {
 	return (
-		<StyledFooter>
-		</StyledFooter>
+		<StyledFooter><p>&copy;2021 All Rights Reserved: <strong>Juan Quirós Mora</strong></p></StyledFooter>
 	);
 }
